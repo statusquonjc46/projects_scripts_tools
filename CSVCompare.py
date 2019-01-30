150be87e1f6043e5aceebdf7e26d9e5e0ac2f7fd
Nicholas Coletta <nicholascoletta@Nicholass-MacBook-Pro-2.local>
2019-01-30 10:29:15 -0500

    inTest.py sucks ass and was coded poorly. As you can see from the mods. CSVCompare.py is the improved version. Use that one if you want to get anything done.

---
+++ CSVCompare.py
@@ -0,0 +1,49 @@
+import csv
+import copy
+
+newData = open("test.csv", "r")
+dataBase = open("test1.csv", "r")
+
+testList = list(newData)
+test1List = list(dataBase)
+preclin = []
+cmp = []
+newData.close()
+dataBase.close()
+
+notInBoth = []
+
+for i in testList:
+    new = i.strip()
+    preclin.append(new)
+for j in test1List:
+    new = j.strip()
+    cmp.append(new)
+
+notInBoth = copy.deepcopy(preclin)
+
+for a in preclin:
+    for b in cmp:
+        print(a, b)
+        if a[0] == b[0]:
+            if a[:4] in b[:4]:
+                userinput = input("Are these the same company: [" + a + "] and [" + b + "] [y for yes, n for no]\n")
+                if userinput == "y":
+                    print("--------------------------------------------------------------\n")
+                    print("["a + "] has been confirmed as the same company as [" + b + "]\n")
+                    print("--------------------------------------------------------------\n")
+                    notInBoth.remove(a)
+                if userinput == "n":
+                    print("----------------------------------------------------------\n")
+                    print("These companies do not match. Continuing matching process.\n")
+                    print("----------------------------------------------------------\n")
+
+print("All comparisons complete, creating new CSV of companies not in our database.\n")
+
+csvFile = open('NotInDatabase.csv', 'w')
+writer = csv.writer(csvFile)
+for item in notInBoth:
+    writer.writerow([item])
+csvFile.close()
+
+print("CSV creation complete. Exiting...")
--- inTest.py
+++ inTest.py
@@ -1,14 +1,15 @@
 import csv
+import copy
 
-test = open("test.csv", "r")
-test1 = open("test1.csv", "r")
+newData = open("test.csv", "r")
+dataBase = open("test1.csv", "r")
 
-testList = list(test)
-test1List = list(test1)
+testList = list(newData)
+test1List = list(dataBase)
 preclin = []
-biosafety = []
-test.close()
-test1.close()
+cmp = []
+newData.close()
+dataBase.close()
 
 notInBoth = []
 
@@ -17,49 +18,78 @@ for i in testList:
     preclin.append(new)
 for j in test1List:
     new = j.strip()
-    biosafety.append(new)
-    
+    cmp.append(new)
+
+notInBoth = copy.deepcopy(preclin)
+
 for a in preclin:
-    for b in biosafety:
-        if a[:3] in b:
-            if a[:3] == "Bio" or a[:3] == "Gen" or a[:3] == "Cor" or a[:3] == "Phar":
-                if a[:5] in b:
-                    userinput = input("Are these similar: " + a + " and " + b + " y for yes, n for no\n")
-                    if userinput == "y":
-                        print(a + " has been confirmed as the same company as " + b + "\n")
-                        if a in notInBoth:
-                            notInBoth.remove(a)
-                            break
-                    if userinput == "n":
-                        print("These companies do not match. Continuing matching process.\n")
-                        if a not in notInBoth:
-                            notInBoth.append(a)
-                            print("Added to list of companies not in our database.\n")
-                        else:
-                            print("Already exists, not added\n")
-            else:
-                userinput = input("Are these similar: " + a + " and " + b + " y for yes, n for no\n")
+    for b in cmp:
+        print(a, b)
+        if a[0] == b[0]:
+            if a[:4] in b[:4]:
+                userinput = input("Are these the same company: " + a + " and " + b + " [y for yes, n for no]\n")
                 if userinput == "y":
                     print(a + " has been confirmed as the same company as " + b + "\n")
-                    if a in notInBoth:
-                        notInBoth.remove(a)
-                        break
+                    notInBoth.remove(a)
                 if userinput == "n":
                     print("These companies do not match. Continuing matching process.\n")
-                    if a not in notInBoth:
-                            notInBoth.append(a)
-                            print("Added to list of companies not in our database.\n")
-                    else:
-                        print("Already exists, not added\n")
-        else:
-            if a not in notInBoth:
-                notInBoth.append(a)
-                print("Added to list of companies not in our database.\n")
-            else:
-                print("Already exists, not added\n")
+print(preclin)
+                            
+            
+#for a in preclin:
+#    for b in cmp:
+#        print(a, b)
+#        if a[:1] in b[:1]:
+#            if a[:5] in b[:5]:
+#                if a[:3] == "Bio" or a[:4] == "Gene" or a[:3] == "Cor" or a[:4] == "Phar":
+#                    if a[:6] in b[:6]:
+#                        userinput = input("Are these similar: " + a + " and " + b + " y for yes, n for no\n")
+#                        if userinput == "y":
+#                            print(a + " has been confirmed as the same company as " + b + "\n")
+#                            if a in notInBoth:
+#                                notInBoth.remove(a)
+#                                break
+#                        if userinput == "n":
+#                            print("These companies do not match. Continuing matching process.\n")
+#                            if a not in notInBoth:
+#                                notInBoth.append(a)
+#                                print("Added to list of companies not in our database.\n")
+#                                break
+#                            else:
+#                                print(a, "\n")
+#                                print("Already exists, not added\n")
+#                else:
+#                    userinput = input("Are these similar: " + a + " and " + b + " y for yes, n for no\n")
+#                    if userinput == "y":
+#                        print(a + " has been confirmed as the same company as " + b + "\n")
+#                        if a in notInBoth:
+#                            notInBoth.remove(a)
+#                            break
+#                    if userinput == "n":
+#                        print("These companies do not match. Continuing matching process.\n")
+#                        if a not in notInBoth:
+#                            notInBoth.append(a)
+#                            print("Added to list of companies not in our database.\n")
+#                            break
+#                        else:
+#                            print(a, "\n")
+#                            print("Already exists, not added\n")
+#            else:
+#                if a not in notInBoth:
+#                    notInBoth.append(a)
+#                    print("Added to list of companies not in our database.\n")
+#                else:
+#                    print("1")
+#                    print(a, " ", b, "\n")
+#                    print("Already exists, not added\n")
+#        else:
+#            if a not in notInBoth:
+#                notInBoth.append(a)
+#                print("Added to list of companies not in our database.\n")
+#            break
 
-print(notInBoth)
-
+#print(notInBoth)
+#
 csvFile = open('NotInDatabase.csv', 'w')
 writer = csv.writer(csvFile)
 for item in notInBoth:
