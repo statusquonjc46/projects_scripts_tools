ad71f1e678670b8fec3c69cbd7a94ba869881003
Nicholas Coletta <nicholascoletta@Nicholass-MacBook-Pro-2.local>
2019-02-23 13:32:10 -0500

    added decryptADirectorino function. Future plans to create encrypted log files for keys to dirs/files. Not sure how secure that is...

--- encryptDATshit.py
+++ encryptDATshit.py
@@ -27,7 +27,7 @@ def encryptorino():
 def decryptorino():
     dFile = input('Which file do you wish to decrypt[Please provide the full file path]:  ')
     dKey = input('Please enter the key you used to encrypt this file:  ')
-    outF = "new" + dFile[:4]
+    outF = 'new' + dFile[:4]
     pyAesCrypt.decryptFile(dFile, outF, dKey, bufferSize)
     return 'File decrypted.'
 
@@ -40,13 +40,27 @@ def encryptADirectorino():
     print('Creating .tar file...')
     os.system('tar -cvf ./' + tarF + ' --directory=' + ePath + ' ' + eFolder)
     eFile = ePath + '/' + tarF
-    outF = tarF + 'crypt'
+    outF = tarF[:-3] + 'cryptorino'
     print('Encrypting .tar file...')
     pyAesCrypt.encryptFile(eFile, outF, eKey, bufferSize)
     print('Success! ' + outF + ' has been created in ' + ePath)
     print('Deleting original tar file...')
     os.system('rm -f ' + tarF)
-    print('Exiting...')
+
+
+def decryptaDirectorino():
+    dFolder = input('What is the directory you wish to decrypt: ')
+    dPath = input('What is the exact path to the directory: ')
+    dKey = input('Enter the key used to encrypt this directory: ')
+    dir = dPath + '/' + dFolder
+    outF = dFolder[:-10] + 'tar'
+    print('Decrypting directory...')
+    pyAesCrypt.decryptFile(dir, outF, dKey, bufferSize)
+    print('Success! ' + dFolder + ' decrypted.')
+    print('Untaring directory to ' + dPath)
+    os.system('tar -C ' + dPath + ' -xvf ' + outF)
+    print('Deleting .crypt file...')
+    os.system('rm -f ' + dFolder)
 
 
 try:
@@ -75,5 +89,8 @@ for currentArgument, currentValue in arguments:
     elif currentArgument in ('-t', '--direncrypt'):
         print('You chose to encrypt a directory.')
         encryptADirectorino()
+        print('Exiting...')
     elif currentArgument in ('-T', '--dirdecrypt'):
         print('You chose to decrypt a directory.')
+        decryptaDirectorino()
+        print('Exiting...')
