0e542dc844d9468889750ffd2db0fc9cbf30be86
Nicholas Coletta <nicholascoletta@Nicholass-MacBook-Pro-2.local>
2019-02-25 15:55:25 -0500

    Fixed variable names, now pep8. added missing arg output of help. changed all string outputs to f-strings. minor verbosity changes. Thanks to Solero93 at r/codereviews

--- encryptDATshit.py
+++ encryptDATshit.py
@@ -1,5 +1,5 @@
 #!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
-# Simple AES256 File Encryption and Decryption using pyAesCrypt Library
+# Simple AES256 File/Directory Encryption and Decryption using pyAesCrypt Library
 # By Nicholas Coletta, in the year 2019.
 # www.github.com/statusquonjc46 for plenty of other python things that may or may not be useful to you. Also my IT490 full stack project lol.
 
@@ -8,89 +8,111 @@ import getopt
 import sys
 import os
 
-bufferSize = 64 * 1024
-progArgs = sys.argv
-argList = progArgs[1:]
+buffer_size = 64 * 1024
+prog_args = sys.argv
+arg_list = prog_args[1:]
 
-unixOptions = 'hedtT'
-gnuOptions = ['help', 'encrypt', 'decrypt', 'direncrypt', 'dirdecrypt']
+unix_options = 'hedED'
+gnu_options = ['help', 'encrypt', 'decrypt', 'direncrypt', 'dirdecrypt']
 
 
 def encryptorino():
-    eFile = input('Which file do you wish to encrypt[Please provide the full file path]:  ')
-    outF = eFile + '.aes'
-    eKey = input('Please enter the key you wish to use to encrypt the file:  ')
-    pyAesCrypt.encryptFile(eFile, outF, eKey, bufferSize)
-    return 'File ecrypted.'
+    user_encrypt_file = input(
+        'Which file do you wish to encrypt[Please provide the full file path]:  ')
+    output_file_name = f'{user_encrypt_file}.cryptorino'
+    encryption_key = input('Please enter the key you wish to use to encrypt the file:  ')
+    print(f'Encrypting {user_encrypt_file}...')
+    pyAesCrypt.encryptFile(user_encrypt_file, output_file_name, encryption_key, buffer_size)
+    print(f'File encrypted. {user_encrypt_file} ---> {output_file_name}')
 
 
 def decryptorino():
-    dFile = input('Which file do you wish to decrypt[Please provide the full file path]:  ')
-    dKey = input('Please enter the key you used to encrypt this file:  ')
-    outF = 'new' + dFile[:4]
-    pyAesCrypt.decryptFile(dFile, outF, dKey, bufferSize)
-    return 'File decrypted.'
+    user_decrpyt_file = input(
+        'Which file do you wish to decrypt[Please provide the full file path]:  ')
+    encryption_key = input('Please enter the key you used to encrypt this file:  ')
+    output_file_name = f'new{user_decrpyt_file[:-11]}'
+    print(f'Decrypting {user_decrpyt_file}...')
+    pyAesCrypt.decryptFile(user_decrpyt_file, output_file_name, encryption_key, buffer_size)
+    print(f'File decrypted. {user_decrpyt_file} ---> {output_file_name}.')
+    print(f'Deleting {user_decrpyt_file}...')
+    os.system(f'rm {user_decrpyt_file}')
 
 
-def encryptADirectorino():
-    eFolder = input('Which directory would you like to encrypt: ')
-    ePath = input('What is the path to the directory: ')
-    eKey = input('Please enter a key you wish to use to encrypt this directory: ')
-    tarF = eFolder + '.tar'
-    print('Creating .tar file...')
-    os.system('tar -cvf ./' + tarF + ' --directory=' + ePath + ' ' + eFolder)
-    eFile = ePath + '/' + tarF
-    outF = tarF[:-3] + 'cryptorino'
-    print('Encrypting .tar file...')
-    pyAesCrypt.encryptFile(eFile, outF, eKey, bufferSize)
-    print('Success! ' + outF + ' has been created in ' + ePath)
-    print('Deleting original tar file...')
-    os.system('rm -f ' + tarF)
+def encrypt_a_directorino():
+    user_encrypt_directory = input('Which directory would you like to encrypt: ')
+    encrypt_directory_path = input('What is the path to the directory: ')
+    encryption_key = input('Please enter a key you wish to use to encrypt this directory: ')
+    tar_file_name = f'{user_encrypt_directory}.tar'
+    print(f'Creating {tar_file_name} file...')
+    os.system(f'tar -cvf ./{tar_file_name} --directory={encrypt_directory_path} {user_encrypt_directory}')
+    full_path_to_directory = f'{encrypt_directory_path}/{tar_file_name}'
+    output_file_name = f'{tar_file_name[:-3]}cryptorino'
+    print(f'Encrypting {tar_file_name} file...')
+    pyAesCrypt.encryptFile(full_path_to_directory, output_file_name, encryption_key, buffer_size)
+    print(f'Success! {output_file_name} has been created in {encrypt_directory_path}')
+    print(f'Deleting {tar_file_name} file...')
+    os.system(f'rm {tar_file_name}')
 
 
-def decryptaDirectorino():
-    dFolder = input('What is the directory you wish to decrypt: ')
-    dPath = input('What is the exact path to the directory: ')
-    dKey = input('Enter the key used to encrypt this directory: ')
-    dir = dPath + '/' + dFolder
-    outF = dFolder[:-10] + 'tar'
-    print('Decrypting directory...')
-    pyAesCrypt.decryptFile(dir, outF, dKey, bufferSize)
-    print('Success! ' + dFolder + ' decrypted.')
-    print('Untaring directory to ' + dPath)
-    os.system('tar -C ' + dPath + ' -xvf ' + outF)
-    print('Deleting .crypt file...')
-    os.system('rm -f ' + dFolder)
+def decrypt_a_directorino():
+    user_decrypt_directory = input('What is the directory you wish to decrypt: ')
+    decrypt_path = input('What is the exact path to the directory: ')
+    encryption_key = input('Enter the key used to encrypt this directory: ')
+    full_path_to_directory = f'{decrypt_path}/{user_decrypt_directory}'
+    output_file_name = f'{user_decrypt_directory[:-10]}tar'
+    print(f'Decrypting {user_decrypt_directory}...')
+    pyAesCrypt.decryptFile(full_path_to_directory, output_file_name, encryption_key, buffer_size)
+    print(f'Success! {user_decrypt_directory} decrypted.')
+    print(f'Untaring directory to {decrypt_path}')
+    os.system(f'tar -C {decrypt_path} -xvf {output_file_name}')
+    print(f'Deleting {user_decrypt_directory} and {output_file_name} file...')
+    os.system(f'rm {user_decrypt_directory} {output_file_name}')
 
 
 try:
-    arguments, values = getopt.getopt(argList, unixOptions, gnuOptions)
+    arguments, values = getopt.getopt(arg_list, unix_options, gnu_options)
 except getopt.error as err:
     print (str(err))
     sys.exit(2)
 
-for currentArgument, currentValue in arguments:
-    if currentArgument in ('-h', '--help'):
-        print('-------------------------------------------------')
-        print('long argument   short argument  Description')
-        print('-------------------------------------------------')
-        print('--help           -h             Prints Help')
-        print('--encrypt        -e             Encrypt File')
-        print('--decrypt        -d             Decrypt File')
-        print('--direncrypt     -t             Encrypt Directory')
-        print('--dirdecrypt     -T             Decrypt Directory')
-        print('-------------------------------------------------')
-    elif currentArgument in ('-e', '--encrypt'):
+for current_argument, current_value in arguments:
+    if current_argument in ('-h', '--help'):
+        print("""
+        -------------------------------------------------
+        long argument   short argument  Description
+        -------------------------------------------------
+        --help           -h             Prints Help
+        --encrypt        -e             Encrypt File
+        --decrypt        -d             Decrypt File
+        --direncrypt     -E             Encrypt Directory
+        --dirdecrypt     -D             Decrypt Directory
+        -------------------------------------------------
+        """)
+    elif current_argument in ('-e', '--encrypt'):
         print ('You chose to encrypt a file.')
         encryptorino()
-    elif currentArgument in ('-d', '--decrypt'):
+        print('Exiting...')
+    elif current_argument in ('-d', '--decrypt'):
         print ('You chose to decrypt a file.')
         decryptorino()
-    elif currentArgument in ('-t', '--direncrypt'):
+        print('Exiting...')
+    elif current_argument in ('-E', '--direncrypt'):
         print('You chose to encrypt a directory.')
-        encryptADirectorino()
+        encrypt_a_directorino()
         print('Exiting...')
-    elif currentArgument in ('-T', '--dirdecrypt'):
+    elif current_argument in ('-D', '--dirdecrypt'):
         print('You chose to decrypt a directory.')
-        decryptaDirectorino()
+        decrypt_a_directorino()
         print('Exiting...')
+    else:
+        print("""
+        -------------------------------------------------
+        long argument   short argument  Description
+        -------------------------------------------------
+        --help           -h             Prints Help
+        --encrypt        -e             Encrypt File
+        --decrypt        -d             Decrypt File
+        --direncrypt     -E             Encrypt Directory
+        --dirdecrypt     -D             Decrypt Directory
+        -------------------------------------------------
+        """)
