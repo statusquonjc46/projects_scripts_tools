37cfceb7d9efdf0760d15c71f9ff3ea58d1ba294
Nicholas Coletta <nicholascoletta@Nicholass-MacBook-Pro-2.local>
2019-03-28 16:16:15 -0400

    added if statement for checking whether an ip was passed or not.

--- ipsweep.sh
+++ ipsweep.sh
@@ -3,7 +3,13 @@
 # By Nicholas Coletta, in the year 2019.
 # www.github.com/statusquonjc46 for plenty of other python things that may or may not be useful to you. Also my IT490 full stack project lol.
 
+if [ "$1" == "" ]
+then
+echo "You forgot to enter a Network address!"
+echo "Syntax: ./ipsweep.sh 192.168.1. [For example, 192.168.1 should be replaced with desired network to sweep]"
 
+else
 for ip in `seq 1 254`; do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
+fi
